bf6ae5e98fe20d5ad0d606c4db680b697be9e09b
Jitendra Singh <jitendrasingh07022004@gmail.com>
2026-08-23 20:24:41 +0530

    Convert to war packaging and update pom.xml

--- src/main/java/Cricket_Tournament/CricketTournamentApplication.java
+++ src/main/java/Cricket_Tournament/CricketTournamentApplication.java
@@ -2,13 +2,18 @@ package Cricket_Tournament;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(scanBasePackages = {"Cricket_Tournament"})
-public class CricketTournamentApplication {
+@SpringBootApplication
+public class CricketTournamentApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(CricketTournamentApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CricketTournamentApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(CricketTournamentApplication.class, args);
+    }
 }
